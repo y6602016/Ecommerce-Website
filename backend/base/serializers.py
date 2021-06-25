2494a815cce0fe68f8e96d718d9268fa358c1b1a
mike <y6602016@gmail.com>
2021-06-25 12:18:32 +0800

    add JWT

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,7 +1,41 @@
-from django.db.models import fields
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Product
+from .models import Product, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["_id", "username", "email", "name", "isAdmin"]
+
+    def get__id(self, instance):
+        return instance.id
+
+    def get_isAdmin(self, instance):
+        return instance.is_staff
+
+    def get_name(self, instance):
+        name = instance.first_name + " " + instance.last_name
+        if not name:
+            name = instance.email
+        return name
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["_id", "username", "email", "name", "isAdmin", "token"]
+
+    def get_token(self, instance):
+        token = RefreshToken.for_user(instance)
+        return str(token.access_token)
 
 
 class ProductSerializer(serializers.ModelSerializer):
--- backend/base/views.py
+++ backend/base/views.py
@@ -6,7 +6,25 @@ from rest_framework.response import Response
 
 from .models import Product
 from .products import products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(["GET"])
@@ -22,6 +40,13 @@ def getRoutes(request):
         "/api/products/<update>/<id>/",
     ]
     return Response(routes)
+
+
+@api_view(["GET"])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
